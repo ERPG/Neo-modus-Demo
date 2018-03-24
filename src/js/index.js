4cc441c67055d31a89566e8a349ba458d1153e45
Ernesto Parra <e.parra@whisbi.com>
2018-03-24 16:20:36 +0100

    sending email with javascript

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,29 @@
 (function () {
-  console.log("ready!");
-  const iframeT = document.getElementById("main-iframe")
-  //.contentWindow.document.documentElement.outerHTML
-  //console.log(iframeT);
+
+  function sendEmail(name, phone, email, message, direction) {
+    $.ajax({
+      url: "",
+      crossDomain: true,
+      dataType: "jsonp",
+      data: {
+        accion: "sendEmail",
+        emailTitle: "Solicitud de informacion",
+        emailTitleFrom: "",
+        greeting: "Tienes una solicitud por gestionar:",
+        name: name,
+        phone: phone,
+        email: email,
+        date: new Date.now(),
+        body: message,
+        emailTo: direction
+      },
+      success: function (result) {
+        /*alert(result[0].status);*/
+      },
+      error: function (xhr, ajaxOptions, thrownError) {
+        /*alert('Error' + xhr + "****" + ajaxOptions + "****" + thrownError);*/
+      }
+    });
+  }
+
 }());
